c2fed141cfde4cd5a173a748df0744d20702f5a2
coiluck <wvy.wingle@gmail.com>
2025-12-10 00:11:50 +0900

    Add: logo animation

---
+++ README.md
@@ -0,0 +1,20 @@
+## 概要
+Agri TraderはTauriを使用して制作された、借金返済 × 農業 × ローグライト × 株 のゲームです。
+
+## 開発
+### インストール
+git、npm、Visual Studio Build Tools、Rustが必要です。
+```bash
+git clone https://github.com/coiluck/agri-trader.git
+npm install
+```
+
+### プレビュー
+開発サーバーを起動し、プレビュー画面を開きます。
+```bash
+npm run tauri dev
+```
+
+### ビルド
+わからない
+後で書く
---
+++ src/js/modules/changeModal.js
@@ -0,0 +1,48 @@
+// modules/changeModal.js
+export function changeModal(modalName, scrollContainer, duration = 500, isFlex = false) {
+  // すべてのモーダルを閉じる
+  document.querySelectorAll('.modal').forEach(function(modal) {
+    modal.classList.remove('fade-in');
+    modal.classList.add('fade-out')
+  });
+  setTimeout(function() {
+    // targetモーダルを表示
+    document.querySelectorAll('.modal').forEach(function(modal) {
+      modal.style.display = 'none';
+    });
+    document.getElementById(`modal-${modalName}`).classList.remove('fade-out');
+    if (isFlex) {
+      document.getElementById(`modal-${modalName}`).style.display = 'flex';
+    } else {
+      document.getElementById(`modal-${modalName}`).style.display = 'block';
+    }
+    // スクロールをリセット（表示されていないとダメみたい）
+    if (scrollContainer) {
+      document.querySelector(`${scrollContainer}`).scrollTop = 0;
+    }
+    document.getElementById(`modal-${modalName}`).classList.add('fade-in');
+  }, duration);
+}
+
+// 上から重ねるタイプのmodal表示
+export function showModal(modalName, scrollContainer, isFlex = false) {
+  if (!isFlex){
+    document.getElementById(`modal-${modalName}`).style.display = 'block';
+  } else {
+    document.getElementById(`modal-${modalName}`).style.display = 'flex';
+  }
+  document.getElementById(`modal-${modalName}`).style.zIndex = '100';
+  document.getElementById(`modal-${modalName}`).classList.remove('fade-out');
+  document.getElementById(`modal-${modalName}`).classList.add('fade-in');
+  if (scrollContainer) {
+    document.querySelector(`${scrollContainer}`).scrollTop = 0;
+  }
+}
+export function closeModal(modalName) {
+  document.getElementById(`modal-${modalName}`).classList.remove('fade-in');
+  document.getElementById(`modal-${modalName}`).classList.add('fade-out');
+  setTimeout(() => {
+    document.getElementById(`modal-${modalName}`).style.display = 'none';
+    document.getElementById(`modal-${modalName}`).style.zIndex = '0';
+  }, 500);
+}
